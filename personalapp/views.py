c9196d2273a480f0b96f2f35dbf29283aeb498b9
sharonchiru <sharonchiru25@gmail.com>
2024-02-12 21:05:53 +0300

    worked with content blocks

--- personalapp/views.py
+++ personalapp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def home_screen(request):
     print(request.headers)
-    return render(request,'base.html',{})
+    return render(request,'mysite/home.html',{})
